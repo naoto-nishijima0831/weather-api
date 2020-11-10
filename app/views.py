a741601494e58e2c22eaac559b18d3ace74a3767
naoto-nishijima0831 <n.nishijima0831@gmail.com>
2020-11-09 17:57:37 -0800

    add exception

--- app/views.py
+++ app/views.py
@@ -12,16 +12,24 @@ from django.db.models import Count
 class WeatherViewSet(viewsets.ViewSet):
 
     def list(self, request):
-        start = (request.GET['start'])
-        end = (request.GET['end'])
+        try:
+            start = request.GET['start']
+            end = request.GET['end']
 
-        target = (request.GET['target'])
+            period = request.GET['period']
 
-        if (target == 'precipitation' or target == 'daylight'):
+            target = request.GET['target']
+
+            if (period != 'monthly' and period != 'weekly' and period != 'daily'):
+                return Response({'result' : 'error'})
+
+            if (target != 'precipitation' and target != 'daylight'):
+                return Response({'result' : 'error'})
+            
             queryset = Weather.objects.raw(
                 'select id, avg(' + target + ') as avg from app_weather where date <= "' + end + '" and date >= "' + start + '"'
             )
-            serializer = WeatherSerializer(queryset, many=True)
-            return Response({'result' : queryset[0].avg})
-        else:
+
+            return Response({'result' : round(queryset[0].avg, 2)})
+        except:
             return Response({'result' : 'error'})
