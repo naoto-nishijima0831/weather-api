f6cdb857a097ca43980e1c6846174d0c8bd6cb40
naoto-nishijima0831 <n.nishijima0831@gmail.com>
2020-11-24 17:44:05 -0800

    ・画面からparamを受け取りAPIを叩いて結果を取得する。

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+
+
 def index(request):
     return render(request, 'weather/weather.html')
 
 def api(request):
-    print(request.POST.get('from-date'))
-    print(request.POST.get('to-date'))
-    print(request.POST.get('target'))
-    print(request.POST.get('area'))
+    params = {
+                'from_date' : request.POST.get('from-date'), 
+                'to_date'   : request.POST.get('to-date'), 
+                'target'    : request.POST.get('target'), 
+                'area'      : request.POST.get('area'), 
+                'period'    : request.POST.get('period'),
+            }
+    result = requests.get('http://127.0.0.1:8000/weather/summary', params=params).json()
+    print(result)
     return render(request, 'weather/weather.html')
