240b1090b3f0111b537054e879c17662c5f9a935
naoto-nishijima0831 <n.nishijima0831@gmail.com>
2020-11-24 23:09:14 -0800

    ・更新ボタンを押した後に検索値がクリアされてしまう事象を解消

--- weather/views.py
+++ weather/views.py
@@ -17,6 +17,11 @@ def api(request):
     horizontal = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
     result = requests.get('http://127.0.0.1:8000/weather/summary', params=params).json()
     return render(request, 'weather/weather.html', {
+        'from_date'    : request.POST.get('from-date'), 
+        'to_date'      : request.POST.get('to-date'), 
+        'target'       : request.POST.get('target'), 
+        'area'         : request.POST.get('area'), 
+        'period'       : request.POST.get('period'),
         'api_result': result,
         'horizontal'   : horizontal,
     })
