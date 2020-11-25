eb8d0cd604841f42720576d66913b6547c3212e6
naoto-nishijima0831 <n.nishijima0831@gmail.com>
2020-11-24 22:44:12 -0800

    ・chart.jsをimport ・APIから取得したresponseをグラフ表示

--- weather/views.py
+++ weather/views.py
@@ -8,11 +8,15 @@ def index(request):
 
 def api(request):
     params = {
-                'from_date' : request.POST.get('from-date'), 
-                'to_date'   : request.POST.get('to-date'), 
-                'target'    : request.POST.get('target'), 
-                'area'      : request.POST.get('area'), 
-                'period'    : request.POST.get('period'),
+                'from_date'    : request.POST.get('from-date'), 
+                'to_date'      : request.POST.get('to-date'), 
+                'target'       : request.POST.get('target'), 
+                'area'         : request.POST.get('area'), 
+                'period'       : request.POST.get('period'),
             }
+    horizontal = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
     result = requests.get('http://127.0.0.1:8000/weather/summary', params=params).json()
-    return render(request, 'weather/weather.html', {'api_result': result})
+    return render(request, 'weather/weather.html', {
+        'api_result': result,
+        'horizontal'   : horizontal,
+    })
